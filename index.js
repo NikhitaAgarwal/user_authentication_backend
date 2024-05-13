46a88cabb8e11e60495d7ad3123aa9ae322ea2df
Nikhita Agarwal <96190789+NikhitaAgarwal@users.noreply.github.com>
2024-05-13 20:14:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const startServer = async () => {
   // Middleware
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(cors());
+  app.use(cors({
+    origin: ["http://localhost:3000/","https://user_register.onrender.com"],
+  }));
 
   // Connecting to MongoDB
   try {
